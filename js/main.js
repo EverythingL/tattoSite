c0996dceab8baca326a4eb9e3313effbdc310c0b
EverythingL <nutter4you.goodslow@gmail.com>
2021-04-03 23:05:51 +0300

    add form, normal burger and language changer

--- js/main.js
+++ js/main.js
@@ -1,20 +1,66 @@
-let btn = document.querySelector('button');
 
-let menu = document.querySelector('.header-menu');
+const $ = (selector) => document.querySelector(selector)
 
-let logo = document.querySelector('.header-logo')
+const headerBurger = document.querySelector('.header-burger');
 
-let body = document.querySelector('body');
+const headerList = $('.header-list');
 
-let span = document.querySelectorAll('span');
+const body = $('body');
 
-btn.onclick = () => {
-	menu.classList.toggle('active');
-	logo.classList.toggle('active');
+let opacity = 0;
+
+let intervalId = 0;
+
+// function fadeOut() {
+// 	intervalId = setInterval(show, 200);
+// }
+// function fadeIn() {
+// 	intervalId = setInterval(hide, 200);
+// }
+
+// function hide() {
+// 	opacity = Number(window.getComputedStyle(headerList).getPropertyValue('opacity'));
+// 	if (opacity > 0) {
+// 		console.log(opacity)
+// 		opacity -= 0.1;
+// 		headerList.style.opacity = opacity;
+
+// 	} else {
+// 		clearInterval(intervalId);
+// 	}
+// }
+// function show() {
+// 	opacity = Number(window.getComputedStyle(headerList).getPropertyValue('opacity'));
+// 	if (opacity <= 1) {
+// 		console.log(opacity)
+// 		opacity += 0.2;
+// 		headerList.style.opacity = opacity;
+
+// 	} else {
+// 		clearInterval(intervalId);
+// 	}
+// }
+
+
+headerBurger.addEventListener('click', (event) => {
+	// const target = event.target;
+	headerList.classList.toggle('active');
+	headerBurger.classList.toggle('active');
 	body.classList.toggle('lock');
+		// if (!target.classList.contains('active')) {
+		// 	// fadeOut();	
+		// } else {
+		// 	headerList.classList.remove('active');
+		// 	headerBurger.classList.remove('active');
+		// 	body.classList.remove('lock');
+		// 	// fadeIn();
+		// }
+});
 
-	for (let i = 0; i < span.length; i++) {
-		span[i].classList.toggle('active');
-	}
 
+window.onload = () => {
+	const preloader = $('.container-preloader');
+	preloader.style.display = 'none';
+	$('.wrapper').style.display = 'block';
+	body.style.backgroundImage = `url(img/about.JPG)`;
 }
