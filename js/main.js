4de0f25a2cff60e37563d459f3c4f67e3e2ad956
EverythingL <nutter4you.goodslow@gmail.com>
2021-04-13 15:29:12 +0300

    edit gallery

--- js/main.js
+++ js/main.js
@@ -11,50 +11,11 @@ let opacity = 0;
 
 let intervalId = 0;
 
-// function fadeOut() {
-// 	intervalId = setInterval(show, 200);
-// }
-// function fadeIn() {
-// 	intervalId = setInterval(hide, 200);
-// }
-
-// function hide() {
-// 	opacity = Number(window.getComputedStyle(headerList).getPropertyValue('opacity'));
-// 	if (opacity > 0) {
-// 		console.log(opacity)
-// 		opacity -= 0.1;
-// 		headerList.style.opacity = opacity;
-
-// 	} else {
-// 		clearInterval(intervalId);
-// 	}
-// }
-// function show() {
-// 	opacity = Number(window.getComputedStyle(headerList).getPropertyValue('opacity'));
-// 	if (opacity <= 1) {
-// 		console.log(opacity)
-// 		opacity += 0.2;
-// 		headerList.style.opacity = opacity;
-
-// 	} else {
-// 		clearInterval(intervalId);
-// 	}
-// }
-
 
 headerBurger.addEventListener('click', (event) => {
-	// const target = event.target;
 	headerList.classList.toggle('active');
 	headerBurger.classList.toggle('active');
 	body.classList.toggle('lock');
-		// if (!target.classList.contains('active')) {
-		// 	// fadeOut();	
-		// } else {
-		// 	headerList.classList.remove('active');
-		// 	headerBurger.classList.remove('active');
-		// 	body.classList.remove('lock');
-		// 	// fadeIn();
-		// }
 });
 
 
@@ -62,5 +23,5 @@ window.onload = () => {
 	const preloader = $('.container-preloader');
 	preloader.style.display = 'none';
 	$('.wrapper').style.display = 'block';
-	body.style.backgroundImage = `url(img/about.JPG)`;
+	body.style.backgroundImage = `url(img/about.jpg)`;
 }
